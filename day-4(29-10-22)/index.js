a15f6ff6b1c3297afcbbc0ef490ac4c09144ee8e
rakeshs10 <rsahano999@gmail.com>
2022-11-23 20:50:39 +0530

    day 4

---
+++ day-4(29-10-22)/index.js
@@ -0,0 +1,354 @@
+// // // // // // // // // // // // // // // // // // // var result = '';
+// // // // // // // // // // // // // // // // // // // var i = 0;
+
+// // // // // // // // // // // // // // // // // // // do {
+// // // // // // // // // // // // // // // // // // //   i = i + 1;
+// // // // // // // // // // // // // // // // // // //   result = result + i;
+// // // // // // // // // // // // // // // // // // // } while (i < 5);
+
+// // // // // // // // // // // // // // // // // // // console.log(result+" ");
+
+// // // // // // // // // // // // // // // // // // // var arr3=[]
+// // // // // // // // // // // // // // // // // // // var arr4=[]
+// // // // // // // // // // // // // // // // // // // console.log(arr3==arr4);
+
+// // // // // // // // // // // // // // // // // //  var arr3='gujh';
+// // // // // // // // // // // // // // // // // //  var arr4='gujbvbgh';
+// // // // // // // // // // // // // // // // // //  var arr5='gugjh';
+
+// // // // // // // // // // // // // // // // // //  console.log(arr3);
+// // // // // // // // // // // // // // // // // // // var arr4=5
+// // // // // // // // // // // // // // // // // // // console.log(arr3==arr4);
+// // // // // // // // // // // // // // // // // let fullName = "rk" + space + "sah"; // concatenation, merging two string together.
+// // // // // // // // // // // // // // // // // console.log(fullName);
+// // // // // // // // // // // // // // // // let name1="rk"
+// // // // // // // // // // // // // // // // let name2="sah"
+// // // // // // // // // // // // // // // // let space=' '
+// // // // // // // // // // // // // // // // let name=name1+space+name2
+// // // // // // // // // // // // // // // // console.log(name);
+// // // // // // // // // // // // // // // const paragraph = "My name is Asabeneh Yetayeh. I live in Finland, Helsinki.\n I am a teacher and I love teaching. I teach HTML, CSS, JavaScript, React, Redux, \
+// // // // // // // // // // // // // // // Node.js, Python, Data Analysis and D3.js for anyone who is interested to learn. \n In the end of 2019, I was thinking to expand my teaching and to reach \
+// // // // // // // // // // // // // // // to global audience and I started a Python challenge from November 20 - December 19.\n It was one of the most rewarding and inspiring experience.\
+// // // // // // // // // // // // // // // Now, we are in 2020. I am enjoying preparing the 30DaysOfJavaScript challenge and \n I hope you are enjoying too."
+
+// // // // // // // // // // // // // // // console.log(paragraph)
+// // // // // // // // // // // // // // console.log('I hope everyone is enjoying the 30 Days Of JavaScript challenge.\nDo you ?') // line break
+// // // // // // // // // // // // // // console.log('Days\tTopics\tExercises')
+// // // // // // // // // // // // // // console.log('Day 1\t3\t5')
+// // // // // // // // // // // // // // console.log('Day 2\t3\t5')
+// // // // // // // // // // // // // // console.log('Day 3\t3\t5')
+// // // // // // // // // // // // // // console.log('Day 4\t3\t5')
+// // // // // // // // // // // // // // console.log('This is a backslash  symbol (\\)') // To write a backslash
+// // // // // // // // // // // // // // console.log('In every programming language it starts with \"Hello, World!\"')
+// // // // // // // // // // // // // // console.log("In every programming language it starts with \'Hello, World!\'")
+// // // // // // // // // // // // // // console.log('The saying \'Seeing is Believing\' isn\'t correct in 2020')
+
+
+
+// // // // // // // // // // // // // // console.log(`The sum of a and b`)              // statically writing the data
+// // // // // // // // // // // // // // let a = 2
+// // // // // // // // // // // // // // let b = 3
+// // // // // // // // // // // // // // console.log(`The sum of ${a} and ${b} is ${a + b}`)
+// // // // // // // // // // // // // // function compare(){
+// // // // // // // // // // // // // //     var num=2;
+// // // // // // // // // // // // // //     const b='2';
+// // // // // // // // // // // // // //     if(a==b)
+// // // // // // // // // // // // // //     return true;
+// // // // // // // // // // // // // //     else
+// // // // // // // // // // // // // //     return false;
+// // // // // // // // // // // // // // }
+// // // // // // // // // // // // // // function compare()
+
+// // // // // // // // // // // // // // {
+
+// // // // // // // // // // // // // //     var num=2;
+
+// // // // // // // // // // // // // //     b= ‘2’;
+
+// // // // // // // // // // // // // //     if(a==b)
+
+// // // // // // // // // // // // // //         return true;
+
+// // // // // // // // // // // // // //     else
+
+// // // // // // // // // // // // // //         return false;
+
+// // // // // // // // // // // // // // }
+
+// // // // // // // // // // // // // function equalto()
+
+// // // // // // // // // // // // // {
+
+// // // // // // // // // // // // //     var num=10;
+
+// // // // // // // // // // // // //     if(num==="10")
+
+// // // // // // // // // // // // //         return true;
+
+// // // // // // // // // // // // //     else
+
+// // // // // // // // // // // // //         return false;
+
+// // // // // // // // // // // // // }
+// // // // // // // // // // // // // var js = (function(x) {return x*x;}(10));
+
+// // // // // // // // // // // // function Geekster(javascript)
+
+// // // // // // // // // // // // {
+
+// // // // // // // // // // // // 	return (javascript ?  “yes” :  “no”);
+
+// // // // // // // // // // // // }
+
+// // // // // // // // // // // // bool ans=true;
+
+// // // // // // // // // // // // console.log(Geekster(ans));
+
+
+// // // // // // // // // // // function height()
+
+// // // // // // // // // // // {	
+
+// // // // // // // // // // //     var  height = 123.56;
+
+// // // // // // // // // // //     var type = (height>=190) ? "tall" : "short";
+
+// // // // // // // // // // //     return type;
+
+// // // // // // // // // // // }
+// // // // // // // // // // // height();
+// // // // // // // // // // // console.log(type);
+
+// // // // // // // // // // function printArray(a) 
+
+// // // // // // // // // // {
+
+// // // // // // // // // //      var len = a.length, i = 0;
+
+// // // // // // // // // //      if (len == 0)
+
+// // // // // // // // // //         console.log("Empty Array");
+
+// // // // // // // // // //      else 
+
+// // // // // // // // // //      {
+
+// // // // // // // // // //          do 
+
+// // // // // // // // // //          {
+
+// // // // // // // // // //              console.log(a[i]);
+
+// // // // // // // // // //          } while (++i < len);
+
+// // // // // // // // // //      }
+
+// // // // // // // // // // }
+// // // // // // // // // var js = 0;
+
+// // // // // // // // // while (js < 10) 
+
+// // // // // // // // // {
+
+// // // // // // // // //      console.log(js);
+
+// // // // // // // // //      js++;
+
+// // // // // // // // // }
+// // // // // // // // function range(int javascript)
+
+// // // // // // // // {
+
+// // // // // // // // 	var a=5;
+
+// // // // // // // // 	for(let i=0;i<javascript;i++)
+
+// // // // // // // // 	{
+
+// // // // // // // // 		console.log(a);
+
+// // // // // // // // 	} 
+
+// // // // // // // // }
+
+// // // // // // // // range(3);
+
+// // // // // // // var scope = "js scope";
+
+// // // // // // // function checkscope() 
+
+// // // // // // // {
+
+// // // // // // //     var scope = "javascript scope"; 
+
+// // // // // // //     function f() 
+
+// // // // // // //     { 
+
+// // // // // // //          return scope; 
+
+// // // // // // //     }
+
+// // // // // // //     return f;
+
+// // // // // // // }
+
+// // // // // // var a=0;
+
+// // // // // // for(a;a<5;a++);
+
+// // // // // // console.log(a);
+
+// // // // // // var book = {
+
+// // // // // //     "main title": "JavaScript", 
+
+// // // // // //     'sub-title': "The Definitive Guide", 
+
+// // // // // //     "for": "all audiences", 
+
+// // // // // //     author: { 
+
+// // // // // //                firstname: "David", 
+
+// // // // // //                surname: "Flanagan" 
+
+// // // // // //             }
+
+// // // // // //  };
+
+// // // // // var count =0;  
+
+// // // // // while (count <10)  
+
+// // // // // {  
+
+// // // // //      console.log(count);  
+
+// // // // //      count++;  
+
+// // // // // }
+
+// // // // var grade='C';  
+
+// // // // var result;  
+
+// // // // switch(grade)  
+
+// // // // {  
+
+// // // // case'A':  
+
+// // // // {  
+
+// // // //         result+="10";  
+
+// // // // break;  
+
+// // // // }  
+
+// // // // case'C':  
+
+// // // // {  
+
+// // // //         result+=" 8";  
+
+// // // // break;  
+
+// // // // }  
+
+// // // // default:  
+
+// // // //     result+=" 0";  
+
+// // // // }  
+
+// // // // console.log(result); 
+
+// // // var grade='Z';  
+
+// // // var result;  
+
+// // // switch(grade)  
+
+// // // {  
+
+// // // case'A':  
+
+// // //         result+="10";  
+
+// // // case'B':  
+
+// // //         result+=" 9";  
+
+// // // default:  
+
+// // //         result+=" 0";  
+
+// // // }  
+
+// // // console.log(result);  
+// // // var x=12;
+
+// // // var y=8;
+
+// // // var res=eval("x+y");
+
+// // // console.log(res);
+// // // (function(){
+
+// // //     setTimeout(()=> console.log(1),2000);
+   
+// // //     console.log(2);
+   
+// // //     setTimeout(()=> console.log(3),0);
+   
+// // //     console.log(4);
+   
+// // //    })();
+// // (function(a){
+
+// //     return (function(){
+   
+// //       console.log(a);
+   
+// //       a = 6;
+   
+// //     })()
+   
+// //    })(21);
+
+// function solve(arr, rotations){
+
+//     if(rotations == 0) return arr;
+   
+//     for(let i = 0; i < rotations; i++){
+   
+//       let element = arr.pop();
+   
+//       arr.unshift(element);
+   
+//     }
+   
+//     return arr;
+   
+//    }
+   
+//  solve([44, 1, 22, 111], 5);
+//  console.log(arr);
+
+var a = 1;  
+
+var b = 0;  
+
+while (a <= 3)  
+
+{  
+
+   a++;  
+
+   b += a * 2;  
+
+   print(b);
+
+}
